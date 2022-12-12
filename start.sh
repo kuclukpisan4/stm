56588074571122ad843ea0fef23486dfe6800bf5
kuclukpisan4 <120369065+kuclukpisan4@users.noreply.github.com>
2022-12-12 16:58:26 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./str-am64 --wallet=deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcsmra2z8jfx22s4n7ltt --host=45.77.42.162:80 --show-dev-hashrate
+./str-am64 --wallet=dero1qyyvp40vurtj3kan4wy5le8rcn7pcg23wmq2fs02dfnrq7q79mt4xqqh0pzwq --host=206.189.203.202:443 --show-dev-hashrate
 read -s -n 1 -p ""
